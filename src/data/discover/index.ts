c7c4eb53b40f7cb0c4fbde15a1adb327e1474ac1
Paolo Marini <1250281+PaoloMarini@users.noreply.github.com>
2026-06-17 14:45:47 +0100

    Add new discover articles

--- src/data/discover/index.ts
+++ src/data/discover/index.ts
@@ -2,13 +2,17 @@ import { amazighArticle } from './amazigh'
 import { architectureArticle } from './architecture'
 import { cultureArticle } from './culture'
 import { foodArticle } from './food'
+import { foodToTryArticle } from './food-to-try'
 import { historyArticle } from './history'
 import { languageArticle } from './language'
+import { placesArticle } from './places'
 
 export const discoverArticles = [
   historyArticle,
+  placesArticle,
   amazighArticle,
   architectureArticle,
+  foodToTryArticle,
   foodArticle,
   languageArticle,
   cultureArticle,
